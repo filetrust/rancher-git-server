e8787dd698aa65b434ab07f681d9635ea022bced
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-09 13:51:30 +0000

    Add script to migrate docker registry url

---
+++ src/update-docker-registry.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd icap-infrastructure
+
+shopt -s dotglob
+
+find * -prune -type d | while IFS= read -r d; do
+    printf "\nCurrent directory is: $d\n\n"
+    cd $d
+
+    if [ -a values.yaml ]
+      then
+	      yq write values.yaml 'imagestore.(registry==*).registry' gwicapcontainerregistry.azurecr.io/ -i
+    fi
+
+    cd ..
+done
+
