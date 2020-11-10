42c1121f233041bf5bb4e4a46b05c7bc5bde27b1
Sylvester Loreto <sylvester.loreto@codurance.com>
2020-11-10 12:16:23 +0000

    update the update-secret.sh to ensure values.yaml in directories at icap level are updated

--- src/update-secrets.sh
+++ src/update-secrets.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-grep '<<.*>>' values.yaml | while read -r line; do
-  echo $line
-  currsecret=$(echo "$line" | sed 's/.*<<\(.*\)>>.*/\1/')
-  echo $currsecret
-  val=$(az keyvault secret show --id $currsecret|  jq -r '.value')
-  echo $val
-  cat values.yaml | sed "s~<<$currsecret>>~$val~g" > newvalues.yaml
-  mv newvalues.yaml values.yaml
+shopt -s dotglob
+
+find * -prune -type d | while IFS= read -r d; do
+    printf "\nCurrent directory is: $d\n\n"
+    cd $d
+
+    grep '<<.*>>' values.yaml | while read -r line; do
+      echo $line
+      currsecret=$(echo "$line" | sed 's/.*<<\(.*\)>>.*/\1/')
+      echo $currsecret
+      val=$(az keyvault secret show --id $currsecret|  jq -r '.value')
+      echo $val
+      cat values.yaml | sed "s~<<$currsecret>>~$val~g" > newvalues.yaml
+      mv newvalues.yaml values.yaml
+    done
+
+    cd ..
 done
+
+
+
+
