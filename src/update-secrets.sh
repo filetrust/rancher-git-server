a79cc93d3b215772157ba7378fdc6cca410371a2
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-11 10:34:57 +0000

    Add script to replace dockerconfig json

--- src/update-secrets.sh
+++ src/update-secrets.sh
@@ -3,6 +3,7 @@
 app_id=$(cat /run/secrets/az-secret | jq -r '.clientId')
 password=$(cat /run/secrets/az-secret | jq -r '.clientSecret')
 tenant_id=$(cat /run/secrets/az-secret | jq -r '.tenantId')
+docker_config=$(cat /run/secrets/az-secret | jq -r '.dockerConfig')
 
 az login --service-principal --username $app_id --password $password --tenant $tenant_id
 
@@ -22,6 +23,21 @@ find * -prune -type d | while IFS= read -r d; do
       val=$(az keyvault secret show --id $currsecret|  jq -r '.value')
       echo $val
       cat values.yaml | sed "s~<<$currsecret>>~$val~g" > newvalues.yaml
+      mv newvalues.yaml values.yaml
+    done
+    cd ..
+done
+
+shopt -s dotglob
+find * -prune -type d | while IFS= read -r d; do
+    printf "\nCurrent directory is: $d\n\n"
+    cd $d
+    grep '##.*##' values.yaml | while read -r line; do
+      echo $line
+      currsecret=$(echo "$line" | sed 's/.*##\(.*\)##.*/\1/')
+      echo $currsecret
+      echo $docker_config
+      cat values.yaml | sed "s~##$currsecret##~$docker_config~g" > newvalues.yaml
       mv newvalues.yaml values.yaml
     done
     cd ..
