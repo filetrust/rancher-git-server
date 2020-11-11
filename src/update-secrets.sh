4a1549c8af405992fcabbc9c4e41680a5cf110e1
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-11 11:00:48 +0000

    Remove duplicate script to replace dockerconfig json

--- src/update-secrets.sh
+++ src/update-secrets.sh
@@ -28,20 +28,5 @@ find * -prune -type d | while IFS= read -r d; do
     cd ..
 done
 
-shopt -s dotglob
-find * -prune -type d | while IFS= read -r d; do
-    printf "\nCurrent directory is: $d\n\n"
-    cd $d
-    grep '##.*##' values.yaml | while read -r line; do
-      echo $line
-      currsecret=$(echo "$line" | sed 's/.*##\(.*\)##.*/\1/')
-      echo $currsecret
-      echo $docker_config
-      cat values.yaml | sed "s~##$currsecret##~$docker_config~g" > newvalues.yaml
-      mv newvalues.yaml values.yaml
-    done
-    cd ..
-done
-
 
 
